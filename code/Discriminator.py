6d4efbb4a31e9ff859a40841d4b6057c1fb5d5f0
Jiseok Yoon <87634620+jsyoonDL@users.noreply.github.com>
2024-03-04 11:47:11 +0900

    Create Discriminator.py

---
+++ code/Discriminator.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 18:26:13 2024
+
+@author: user
+"""
+
+
+import torch.nn as nn
+
+
+
+##############################
+#        Discriminator
+##############################
+
+class Discriminator(nn.Module):
+    def __init__(self, input_shape):
+        super(Discriminator, self).__init__()
+
+        self.input_shape = input_shape
+        in_channels, in_height, in_width = self.input_shape
+        patch_h, patch_w = int(in_height / 2 ** 4), int(in_width / 2 ** 4)
+ 
+        def discriminator_block(in_filters, out_filters, first_block=False):
+            layers = []
+            layers.append(nn.Conv2d(in_filters, out_filters, kernel_size=3, stride=1, padding=1))
+            if not first_block:
+                layers.append(nn.BatchNorm2d(out_filters))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            layers.append(nn.Conv2d(out_filters, out_filters, kernel_size=3, stride=2, padding=1))
+            layers.append(nn.BatchNorm2d(out_filters))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        layers = []
+        in_filters = in_channels
+        for i, out_filters in enumerate([64, 128, 256, 512]):
+            layers.extend(discriminator_block(in_filters, out_filters, first_block=(i == 0)))
+            in_filters = out_filters
+
+        layers.append(nn.Conv2d(out_filters, 1, kernel_size=3, stride=1, padding=1))
+        # layers.append(nn.ReLU(inplace=True))
+        # layers.append(nn.Sigmoid())
+
+        
+        self.model = nn.Sequential(*layers)
+        
+        self.apply(self._init_weights)
+        
+    def _init_weights(self, m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+        elif classname.find('BatchNorm') != -1:
+            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            nn.init.constant_(m.bias.data, 0)
+
+
+
+        
+    def forward(self, img):
+        output = self.model(img)
+        
+        return output
+    
+if __name__ == '__main__':
+    import torch
+    out = Discriminator((3,448,640))(torch.rand(12,3,448,640))
